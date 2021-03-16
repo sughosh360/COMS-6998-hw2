96912778bcf4be15064ef072addff8667f022e8c
Sughosh Vallisha Kaushik <sughosh@dyn-129-236-220-165.dyn.columbia.edu>
2021-03-16 17:58:09 -0400

    Front-End code

---
+++ photoalbum.js
@@ -0,0 +1,176 @@
+$(document).ready(function () {
+    console.log("Ready!");
+
+
+    function callSearchAPI(query) {
+        return sdk.searchGet({ q: query }, {}, {});
+    }
+
+    function callUploadAPI(image, customLabels) {
+        var filename = document.getElementById("image-file").value;
+        console.log(filename);
+        var additionalparams = {
+            headers: {
+                'Content-Type': 'text/html',
+                'realcontenttype': 'jpeg',
+                'customlabels': "panda"
+            },
+            queryParams: {
+                'objectKey': document.getElementById("image-file").value
+            }
+        }
+        var params = {};
+        console.log(additionalparams);
+        return sdk.uploadPut(params, image, additionalparams);
+    }
+
+    function search() {
+        var search_query = $('.search-query').val()
+        console.log("Searching for " + search_query);
+        $('.search-results').html("");
+        callSearchAPI(search_query).then((response) => {
+            var image_list = response.data.results;
+            console.log(image_list);
+            
+            var html = '<br>';
+            if (image_list.length == 0) {
+                html += '<h3>No images found for given tags</h3>'
+            } else {
+                var i;
+                for (i = 0; i < image_list.length; i++) {
+                    html += '<img src="' + image_list[i].url + '" witdth="250" height="250" class="thumbnail" /><br><br>'
+                }
+            }
+            console.log(html);
+            $(html).appendTo($('.search-results'));
+        });
+    }
+
+    function upload() {
+        var img = document.getElementById("imagebase64").value;
+        var comma_index = img.indexOf(",");
+        var image_str = img.substring(comma_index+1);
+        console.log(image_str);
+        callUploadAPI(image_str, "").then((response) => {
+            console.log(response);
+        });
+    }
+
+    $('.search-button').click(function () {
+        search();
+    });
+
+    $('.upload-button').click(function () {
+        upload();
+    });
+
+    $(window).on('keydown', function (e) {
+        if (e.which == 13) {
+            search();
+            return false;
+        }
+    })
+
+    function readFile() {
+  
+        if (this.files && this.files[0]) {
+
+            var FR = new FileReader();
+            console.log(this.files[0]['name']);
+            var filename = this.files[0]['name']
+            document.getElementById("image-file").setAttribute("value", filename);
+            FR.addEventListener("load", function(e) {
+                document.getElementById("img").src = e.target.result;
+                document.getElementById("imagebase64").setAttribute("value", e.target.result);
+            }); 
+
+            FR.readAsDataURL( this.files[0] );
+        }
+        
+    }
+
+    document.getElementById("image").addEventListener("change", readFile);
+
+    $(function () {
+        // check for support (webkit only)
+        if (!('webkitSpeechRecognition' in window)) // todo error
+            return;
+    
+        var input = document.getElementById('input');
+        var record = document.getElementById('record');
+    
+        // setup recognition
+        const talkMsg = 'Speak now';
+        // seconds to wait for more input after last
+        const patience = 5;
+        var recognizing = false;
+        var timeout;
+        var oldPlaceholder = null;
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+    
+        function restartTimer() {
+            timeout = setTimeout(function () {
+                recognition.stop();
+            }, patience * 1000);
+        }
+    
+        recognition.onstart = function () {
+            oldPlaceholder = input.placeholder;
+            input.placeholder = talkMsg;
+            recognizing = true;
+            restartTimer();
+        };
+    
+        recognition.onend = function () {
+            recognizing = false;
+            console.log("onend");
+            clearTimeout(timeout);
+            if (oldPlaceholder !== null)
+                input.placeholder = oldPlaceholder;
+            search();
+        };
+    
+        recognition.onresult = function (event) {
+            clearTimeout(timeout);
+    
+            // get SpeechRecognitionResultList object
+            var resultList = event.results;
+            console.log("onresult");
+            // go through each SpeechRecognitionResult object in the list
+            var finalTranscript = '';
+            var interimTranscript = '';
+            for (var i = event.resultIndex; i < resultList.length; ++i) {
+                var result = resultList[i];
+                // get this result's first SpeechRecognitionAlternative object
+                var firstAlternative = result[0];
+                if (result.isFinal) {
+                    finalTranscript = firstAlternative.transcript;
+                } else {
+                    interimTranscript += firstAlternative.transcript;
+                }
+            }
+            // capitalize transcript if start of new sentence
+            var transcript = finalTranscript || interimTranscript;
+    
+            // append transcript to cached input value
+            input.value = transcript;
+    
+            restartTimer();
+        };
+    
+        record.addEventListener('click', function (event) {
+            event.preventDefault();
+    
+            // stop and exit if already going
+            if (recognizing) {
+                recognition.stop();
+                return;
+            }
+
+            // restart recognition
+            recognition.start();
+        }, false);
+    });
+});
